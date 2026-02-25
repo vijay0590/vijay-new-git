cfab61cc4a8b92593dcad25da48c892deb7247bc
vijay0590 <thilagav06@gmail.com>
2026-02-24 18:07:03 -0800

    passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-function App(){
+function Header({name , age}){
   return(
-    <h1>hello world!</h1>
+    <h1>helllo {name } you are {age}</h1>
+  )
+
+}
+function App(){
+  let name= "vijay"
+  let age =35
+  return(
+<Header name={name}  age={age}></Header>
   )
 }
 export default App;
